dc9c270283d1365cd60a685ad74f6cf35ca19953
Joao Vitral <joaoviniciusvitral@hotmail.com>
2025-05-22 14:27:42 -0300

    refatora os serializers de Playlist e Video para utilizar o método to_representation, removendo os campos foto_url e arquivo_url, e garantindo a inclusão da porta 8081 nas URLs absolutas

--- aulas/serializers.py
+++ aulas/serializers.py
@@ -2,56 +2,50 @@ from rest_framework import serializers
 from .models import Video, User, Playlist
 
 class PlaylistSerializer(serializers.ModelSerializer):
-    foto = serializers.ImageField(required=False)
-    foto_url = serializers.SerializerMethodField()
+    foto = serializers.ImageField(required=False, use_url=True)
     class Meta:
         model = Playlist
-        fields = ['id', 'nome', 'descricao', 'professor', 'videos', 'foto', 'foto_url']
+        fields = ['id', 'nome', 'descricao', 'professor', 'videos', 'foto']
         read_only_fields = ['id', 'professor']
 
-    def get_foto_url(self, obj):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
         request = self.context.get('request')
-        if obj.foto and request:
-            url = request.build_absolute_uri(obj.foto.url)
-            # Garante que a porta 8081 está presente
+        if instance.foto and request:
+            url = request.build_absolute_uri(instance.foto.url)
             if ':8081' not in url:
                 url = url.replace('api.majorssolutions.com.br', 'api.majorssolutions.com.br:8081')
-            return url
-        return None
+            data['foto'] = url
+        return data
 
 class VideoSerializer(serializers.ModelSerializer):
     playlists = PlaylistSerializer(many=True, read_only=True)
     playlists_ids = serializers.PrimaryKeyRelatedField(
         queryset=Playlist.objects.all(), many=True, write_only=True, source='playlists'
     )
-    arquivo = serializers.FileField(required=True)
-    professor_nome = serializers.CharField(source='professor.username', read_only=True)  # Adiciona o nome do professor
-    thumbnail = serializers.ImageField(required=False, allow_null=True, use_url=True)  # Adiciona o campo thumbnail
-    arquivo_url = serializers.SerializerMethodField()
-    thumbnail_url = serializers.SerializerMethodField()
-    
+    arquivo = serializers.FileField(required=True, use_url=True)
+    professor_nome = serializers.CharField(source='professor.username', read_only=True)
+    thumbnail = serializers.ImageField(required=False, allow_null=True, use_url=True)
+
     class Meta:
         model = Video
-        fields = ['id', 'titulo', 'descricao', 'arquivo', 'arquivo_url', 'criado_em', 'professor', 'professor_nome', 'playlists', 'playlists_ids', 'thumbnail', 'thumbnail_url']
+        fields = ['id', 'titulo', 'descricao', 'arquivo', 'criado_em', 'professor', 'professor_nome', 'playlists', 'playlists_ids', 'thumbnail']
         read_only_fields = ['id', 'criado_em', 'professor', 'playlists', 'thumbnail']
 
-    def get_arquivo_url(self, obj):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
         request = self.context.get('request')
-        if obj.arquivo and request:
-            url = request.build_absolute_uri(obj.arquivo.url)
+        if instance.arquivo and request:
+            url = request.build_absolute_uri(instance.arquivo.url)
             if ':8081' not in url:
                 url = url.replace('api.majorssolutions.com.br', 'api.majorssolutions.com.br:8081')
-            return url
-        return None
-
-    def get_thumbnail_url(self, obj):
-        request = self.context.get('request')
-        if obj.thumbnail and request:
-            url = request.build_absolute_uri(obj.thumbnail.url)
+            data['arquivo'] = url
+        if instance.thumbnail and request:
+            url = request.build_absolute_uri(instance.thumbnail.url)
             if ':8081' not in url:
                 url = url.replace('api.majorssolutions.com.br', 'api.majorssolutions.com.br:8081')
-            return url
-        return None
+            data['thumbnail'] = url
+        return data
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
