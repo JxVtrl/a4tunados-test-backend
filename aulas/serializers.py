ac5656332ec19a6964780029b24a358b08ffedfe
Joao Vitral <joaoviniciusvitral@hotmail.com>
2025-05-22 14:15:58 -0300

    adiciona campos foto_url e arquivo_url nos serializers de Playlist e Video, respectivamente, para retornar URLs absolutas e garantir a inclusão da porta 8081

--- aulas/serializers.py
+++ aulas/serializers.py
@@ -2,11 +2,22 @@ from rest_framework import serializers
 from .models import Video, User, Playlist
 
 class PlaylistSerializer(serializers.ModelSerializer):
-    foto = serializers.ImageField(required=False) 
+    foto = serializers.ImageField(required=False)
+    foto_url = serializers.SerializerMethodField()
     class Meta:
         model = Playlist
-        fields = ['id', 'nome', 'descricao', 'professor', 'videos', 'foto']
+        fields = ['id', 'nome', 'descricao', 'professor', 'videos', 'foto', 'foto_url']
         read_only_fields = ['id', 'professor']
+
+    def get_foto_url(self, obj):
+        request = self.context.get('request')
+        if obj.foto and request:
+            url = request.build_absolute_uri(obj.foto.url)
+            # Garante que a porta 8081 está presente
+            if ':8081' not in url:
+                url = url.replace('api.majorssolutions.com.br', 'api.majorssolutions.com.br:8081')
+            return url
+        return None
 
 class VideoSerializer(serializers.ModelSerializer):
     playlists = PlaylistSerializer(many=True, read_only=True)
@@ -16,11 +27,21 @@ class VideoSerializer(serializers.ModelSerializer):
     arquivo = serializers.FileField(required=True)
     professor_nome = serializers.CharField(source='professor.username', read_only=True)  # Adiciona o nome do professor
     thumbnail = serializers.ImageField(required=False, allow_null=True, use_url=True)  # Adiciona o campo thumbnail
+    arquivo_url = serializers.SerializerMethodField()
     
     class Meta:
         model = Video
-        fields = ['id', 'titulo', 'descricao', 'arquivo', 'criado_em', 'professor', 'professor_nome', 'playlists', 'playlists_ids', 'thumbnail']
+        fields = ['id', 'titulo', 'descricao', 'arquivo', 'arquivo_url', 'criado_em', 'professor', 'professor_nome', 'playlists', 'playlists_ids', 'thumbnail']
         read_only_fields = ['id', 'criado_em', 'professor', 'playlists', 'thumbnail']
+
+    def get_arquivo_url(self, obj):
+        request = self.context.get('request')
+        if obj.arquivo and request:
+            url = request.build_absolute_uri(obj.arquivo.url)
+            if ':8081' not in url:
+                url = url.replace('api.majorssolutions.com.br', 'api.majorssolutions.com.br:8081')
+            return url
+        return None
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
